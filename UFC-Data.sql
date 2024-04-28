f2ec8b2c1b6c99a67132c1cd7bc2dc0f0f30da8e
MaxBlumenfeld7 <mblumenf@villanova.edu>
2024-04-28 17:10:19 -0400

    Created file for data

---
+++ UFC-Data.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Fighter (
+    Name VARCHAR2(255) PRIMARY KEY,
+    Age NUMBER,
+    Nationality VARCHAR2(255),
+    Style VARCHAR2(255),
+    Gym_Name VARCHAR2(255),
+    Height NUMBER,  -- Height of the fighter in inches
+    Reach NUMBER    -- Reach of the fighter in inches
+);
