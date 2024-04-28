eb97111e8297d89fc897da184859b3afaeaccf76
MaxBlumenfeld7 <mblumenf@villanova.edu>
2024-04-28 17:39:22 -0400

    Started inserting data.

--- UFC-Data.sql
+++ UFC-Data.sql
@@ -1,9 +1,10 @@
-CREATE TABLE Fighter (
-    Name VARCHAR2(255) PRIMARY KEY,
-    Age NUMBER,
-    Nationality VARCHAR2(255),
-    Style VARCHAR2(255),
-    Gym_Name VARCHAR2(255),
-    Height NUMBER,  -- Height of the fighter in inches
-    Reach NUMBER    -- Reach of the fighter in inches
-);
+INSERT INTO WeightClass (Name, NonTitleLimit, TitleLimit)
+VALUES 
+    ('Flyweight', 126, 125),
+    ('Bantamweight', 136, 135),
+    ('Featherweight', 146, 145),
+    ('Lightweight', 156, 155),
+    ('Welterweight', 171, 170),
+    ('Middleweight', 186, 185),
+    ('Light Heavyweight', 206, 205),
+    ('Heavyweight', 266, 265);
