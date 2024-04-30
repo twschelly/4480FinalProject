057214f52b97af547d65005274fa2453ce417b07
twschelly <tschelle@villanova.edu>
2024-04-30 16:43:14 -0400

    Added top 5 contenders for each weight class

--- UFC-Data.sql
+++ UFC-Data.sql
@@ -172,3 +172,324 @@ INSERT INTO Title (FighterName, WeightClassName, TitleStatus)
 VALUES ('Julianna Peña', 'Women Bantamweight', 'Champion');
 
 select * from FIGHTERRECORD;
+
+-- Heavyweight
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Tom Aspinall', 30, 'UK', 'Striking', 'Team Kaobon', 78, 78);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Ciryl Gane', 33, 'France', 'Striking', 'MMA Factory', 77, 83);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Sergei Pavlovich', 31, 'Russia', 'Striking', 'Eagles MMA', 76, 84);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Stipe Miocic', 41, 'USA', 'Striking', 'Strong Style Fight Team', 76, 80);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Curtis Blaydes', 33, 'USA', 'Wrestling', 'Elevation Fight Team', 76, 80);
+
+-- Light Heavyweight
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Jiří Procházka', 31, 'Czech Republic', 'Striking', 'Jetsaam Gym', 76, 80);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Magomed Ankalaev', 31, 'Russia', 'Striking', 'Gor MMA Team', 76, 77);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Jamahal Hill', 32, 'USA', 'Striking', 'Black Lion Jiu-Jitsu', 76, 79);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Jan Błachowicz', 40, 'Poland', 'Striking', 'WCA Fight Team', 74, 78);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Aleksandar Rakic', 31, 'Austria', 'Striking', 'American Top Team', 77, 78);
+
+-- Middleweight
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Sean Strickland', 32, 'USA', 'Striking', 'Xtreme Couture', 73, 76);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Israel Adesanya', 34, 'New Zealand', 'Striking', 'City Kickboxing', 76, 80);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Robert Whittaker', 33, 'New Zealand', 'Striking', 'Gracie Jiu-Jitsu Academy', 73, 73);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Jared Cannonier', 39, 'USA', 'Striking', 'MMA Lab', 73, 77);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Marvin Vettori', 30, 'Italy', 'Striking', 'Kings MMA', 72, 74);
+
+-- Welterweight
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Kamaru Usman', 36, 'Nigeria', 'Striking', 'Sanford MMA', 72, 76);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Belal Muhammad', 35, 'USA', 'Striking', 'Roufusport MMA', 71, 72);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Shavkat Rakhmonov', 29, 'Kazakhstan', 'Striking', 'Sanford MMA', 73, 77);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Colby Covington', 35, 'USA', 'Striking', 'MMA Masters', 71, 72);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Jack Della Maddalena', 26, 'Australia', 'Striking', 'MMA Lab', 71, 73);
+
+-- Lightweight
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Arman Tsarukyan', 27, 'Armenia', 'Striking', 'American Top Team', 71, 72);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Charles Oliveira', 34, 'Brazil', 'Striking', 'Chute Boxe', 71, 74);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Justin Gaethje', 35, 'USA', 'Striking', 'Elevation Fight Team', 71, 71);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Dustin Poirier', 35, 'USA', 'Striking', 'American Top Team', 69, 72);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Mateusz Gamrot', 33, 'Poland', 'Striking', 'American Top Team', 70, 70);
+
+-- Featherweight
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Alexander Volkanovski', 35, 'Australia', 'Striking', 'Freestyle Fighting Gym', 66, 71);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Max Holloway', 32, 'USA', 'Striking', 'Gracie Jiu-Jitsu Academy', 71, 69);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Brian Ortega', 32, 'USA', 'Brazilian Jiu-Jitsu', 'Black House MMA', 68, 69);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Yair Rodriguez', 31, 'Mexico', 'Striking', 'Jackson Wink MMA Academy', 71, 73);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Movsar Evloev', 29, 'Russia', 'Striking', 'Tiger Muay Thai', 69, 72);
+
+-- Bantamweight
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Marlon Vera', 30, 'Ecuador', 'Striking', 'RVCA Training Center', 68, 70);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Petr Yan', 30, 'Russia', 'Striking', 'Tiger Muay Thai', 68, 70);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Cory Sandhagen', 31, 'USA', 'Striking', 'Elevation Fight Team', 70, 70);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Merab Dvalishvili', 33, 'Georgia', 'Striking', 'Serra-Longo Fight Team', 68, 68);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Ricky Simon', 31, 'USA', 'Striking', 'Gracie Barra Portland', 68, 68);
+
+-- Flyweight
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Alexandre Pantoja', 34, 'Brazil', 'Brazilian Jiu-Jitsu', 'American Top Team', 65, 68);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Brandon Moreno', 30, 'Mexico', 'Brazilian Jiu-Jitsu', 'Entram Gym', 67, 70);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Kai Kara-France', 31, 'New Zealand', 'Striking', 'City Kickboxing', 64, 68);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Manel Kape', 30, 'Angola', 'Striking', 'AKA Thailand', 65, 68);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Brandon Royval', 31, 'USA', 'Brazilian Jiu-Jitsu', 'Factory X', 70, 70);
+
+-- Women's Strawweight
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Tatiana Suarez', 33, 'USA', 'Striking', 'Millennia MMA', 66, 67);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Yan Xiaonan', 34, 'China', 'Striking', 'Jackson Wink MMA Academy', 65, 65);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Amanda Lemos', 36, 'Brazil', 'Striking', 'Marajó Brothers Team', 63, 64);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Jéssica Andrade', 32, 'Brazil', 'Striking', 'Paraná Vale Tudo', 61, 62);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Virna Jandiroba', 35, 'Brazil', 'Brazilian Jiu-Jitsu', 'American Top Team', 64, 64);
+
+-- Women's Flyweight
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Manon Fiorot', 33, 'France', 'Striking', 'GFA Team', 65, 67);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Erin Blanchfield', 24, 'USA', 'Brazilian Jiu-Jitsu', 'Renzo Gracie', 65, 67);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Taila Santos', 30, 'Brazil', 'Striking', 'Paraná Vale Tudo', 65, 66);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Jéssica Andrade', 32, 'Brazil', 'Striking', 'Paraná Vale Tudo', 61, 62);
+
+INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Height, Reach)
+VALUES ('Casey ONeill', 25, 'Australia', 'Striking', 'Tiger Muay Thai', 65, 67);
+
+-- Heavyweight
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Tom Aspinall', 'Heavyweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Ciryl Gane', 'Heavyweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Sergei Pavlovich', 'Heavyweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Stipe Miocic', 'Heavyweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Curtis Blaydes', 'Heavyweight', 'Contender');
+
+-- Light Heavyweight
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Jiří Procházka', 'Light Heavyweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Magomed Ankalaev', 'Light Heavyweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Jamahal Hill', 'Light Heavyweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Jan Błachowicz', 'Light Heavyweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Aleksandar Rakic', 'Light Heavyweight', 'Contender');
+
+-- Middleweight
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Sean Strickland', 'Middleweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Israel Adesanya', 'Middleweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Robert Whittaker', 'Middleweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Jared Cannonier', 'Middleweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Marvin Vettori', 'Middleweight', 'Contender');
+
+-- Welterweight
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Kamaru Usman', 'Welterweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Belal Muhammad', 'Welterweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Shavkat Rakhmonov', 'Welterweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Colby Covington', 'Welterweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Jack Della Maddalena', 'Welterweight', 'Contender');
+
+-- Lightweight
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Arman Tsarukyan', 'Lightweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Charles Oliveira', 'Lightweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Justin Gaethje', 'Lightweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Dustin Poirier', 'Lightweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Mateusz Gamrot', 'Lightweight', 'Contender');
+
+-- Featherweight
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Alexander Volkanovski', 'Featherweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Max Holloway', 'Featherweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Brian Ortega', 'Featherweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Yair Rodriguez', 'Featherweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Movsar Evloev', 'Featherweight', 'Contender');
+
+-- Bantamweight
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Marlon Vera', 'Bantamweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Petr Yan', 'Bantamweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Cory Sandhagen', 'Bantamweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Merab Dvalishvili', 'Bantamweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Ricky Simon', 'Bantamweight', 'Contender');
+
+-- Flyweight
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Alexandre Pantoja', 'Flyweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Brandon Moreno', 'Flyweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Kai Kara-France', 'Flyweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Manel Kape', 'Flyweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Brandon Royval', 'Flyweight', 'Contender');
+
+-- Women's Strawweight
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Tatiana Suarez', 'Women Strawweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Yan Xiaonan', 'Women Strawweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Amanda Lemos', 'Women Strawweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Jéssica Andrade', 'Women Strawweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Virna Jandiroba', 'Women Strawweight', 'Contender');
+
+-- Women's Flyweight
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Manon Fiorot', 'Women Flyweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Erin Blanchfield', 'Women Flyweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Taila Santos', 'Women Flyweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Jéssica Andrade', 'Women Flyweight', 'Contender');
+
+INSERT INTO FighterWeightClass (FighterName, WeightClassName, Status)
+VALUES ('Casey ONeill', 'Women Flyweight', 'Contender');
+
