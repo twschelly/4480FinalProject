f11783304db95a9c41dab83cfed1670532481614
MaxBlumenfeld7 <mblumenf@villanova.edu>
2024-05-01 15:18:26 -0400

    Added more inserts

--- NonChampData.sql
+++ NonChampData.sql
@@ -1,4 +1,4 @@
--- Heavyweight
+-- Heavyweight fighter inserts
 INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Current_weight_class, Height, Reach)
 VALUES ('Tom Aspinall', 30, 'UK', 'Striking', 'Team Kaobon', 'Heavyweight',77, 78);
 
@@ -15,7 +15,33 @@ VALUES ('Stipe Miocic', 41, 'USA', 'Striking', 'Strong Style Fight Team','Heavyw
 INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Current_weight_class, Height, Reach)
 VALUES ('Curtis Blaydes', 33, 'USA', 'Wrestling', 'Elevation Fight Team','Heavyweight',76, 80);
 
--- Light Heavyweight
+--Heavyweight FighterRecord Inserts
+-- Insert for Tom Aspinall
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Tom Aspinall', 11, 3, 0, 0, 1, 1, 0, 1, 0, 0);
+
+-- Insert for Ciryl Gane
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Ciryl Gane', 6, 3, 3, 0, 1, 0, 1, 0, 0, 0);
+
+-- Insert for Sergei Pavlovich
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Sergei Pavlovich', 15, 0, 3, 0, 2, 0, 0, 0, 0, 0);
+
+-- Insert for Stipe Miocic
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Stipe Miocic', 15, 0, 5, 0, 2, 0, 2, 0, 0, 0);
+
+-- Insert for Curtis Blaydes
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Curtis Blaydes', 13, 0, 5, 0, 4, 0, 0, 0, 0, 1);
+
+
+
+
+
+
+-- Light Heavyweight Fighter Inserts
 INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Current_weight_class, Height, Reach) --fmr champ
 VALUES ('Jiří Procházka', 31, 'Czech Republic', 'Striking', 'Jetsaam Gym','Light Heavyweight', 75, 80);
 
@@ -31,7 +57,33 @@ VALUES ('Jan Błachowicz', 40, 'Poland', 'Striking', 'WCA Fight Team','Light Hea
 INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Current_weight_class, Height, Reach)
 VALUES ('Aleksandar Rakic', 31, 'Austria', 'Striking', 'American Top Team','Light Heavyweight', 77, 78);
 
--- Middleweight
+--light heavyweight fighter record inserts
+-- Insert for Jiří Procházka
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Jiří Procházka', 26, 3, 1, 0, 3, 1, 0, 0, 1, 0);
+
+-- Insert for Magomed Ankalaev
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Magomed Ankalaev', 10, 0, 8, 0, 1, 0, 0, 0, 1, 1);
+
+-- Insert for Jamahal Hill
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Jamahal Hill', 7, 0, 5, 0, 2, 0, 0, 0, 0, 1);
+
+-- Insert for Jan Błachowicz
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Jan Błachowicz', 9, 9, 11, 0, 2, 2, 6, 0, 1, 0);
+
+-- Insert for Aleksandar Rakic
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Aleksandar Rakic', 9, 1, 4, 0, 2, 1, 1, 0, 0, 0);
+
+
+
+
+
+
+-- Middleweight Fighter Inserts
 INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Current_weight_class, Height, Reach) --fmr champ
 VALUES ('Sean Strickland', 32, 'USA', 'Striking', 'Xtreme Couture', 'Middleweight', 73, 76);
 
@@ -47,7 +99,32 @@ VALUES ('Jared Cannonier', 39, 'USA', 'Striking', 'MMA Lab', 'Middleweight', 73,
 INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Current_weight_class, Height, Reach)
 VALUES ('Marvin Vettori', 30, 'Italy', 'Striking', 'Kings MMA', 'Middleweight', 72, 74);
 
--- Welterweight
+--middleweight FighterRecord Inserts
+-- Insert for Sean Strickland
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Sean Strickland', 11, 4, 13, 0, 2, 0, 4, 0, 0, 0);
+
+-- Insert for Israel Adesanya
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Israel Adesanya', 16, 0, 8, 0, 1, 0, 2, 0, 0, 0);
+
+-- Insert for Robert Whittaker
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Robert Whittaker', 9, 5, 11, 0, 3, 1, 3, 0, 0, 0);
+
+-- Insert for Jared Cannonier
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Jared Cannonier', 10, 2, 5, 0, 2, 0, 4, 0, 0, 0);
+
+-- Insert for Marvin Vettori
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Marvin Vettori', 2, 9, 8, 0, 0, 0, 7, 0, 1, 0);
+
+
+
+
+
+-- Welterweight Fighter Inserts
 INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Current_weight_class, Height, Reach) --fmr champ
 VALUES ('Kamaru Usman', 36, 'Nigeria', 'Striking', 'Sanford MMA', 'Welterweight', 72, 76);
 
@@ -62,8 +139,32 @@ VALUES ('Colby Covington', 35, 'USA', 'Striking', 'MMA Masters', 'Welterweight',
 
 INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Current_weight_class, Height, Reach)
 VALUES ('Jack Della Maddalena', 26, 'Australia', 'Striking', 'MMA Lab', 'Welterweight', 71, 73);
+--welterweight fighterRecord Inserts
+-- Insert for Kamaru Usman
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Kamaru Usman', 9, 1, 10, 0, 1, 1, 2, 0, 0, 0);
 
--- Lightweight
+-- Insert for Belal Muhammad
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Belal Muhammad', 5, 1, 17, 0, 1, 0, 2, 0, 0, 1);
+
+-- Insert for Shavkat Rakhmonov
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Shavkat Rakhmonov', 8, 10, 0, 0, 0, 0, 0, 0, 0, 0);
+
+-- Insert for Colby Covington
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Colby Covington', 4, 4, 9, 0, 1, 1, 2, 0, 0, 0);
+
+-- Insert for Jack Della Maddalena
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Jack Della Maddalena', 12, 2, 3, 0, 1, 1, 0, 0, 0, 0);
+
+
+
+
+
+-- Lightweight Fighter Inserts
 INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Current_weight_class, Height, Reach)
 VALUES ('Arman Tsarukyan', 27, 'Armenia', 'Striking', 'American Top Team', 'Lightweight', 71, 72);
 
@@ -78,8 +179,32 @@ VALUES ('Dustin Poirier', 35, 'USA', 'Striking', 'American Top Team', 'Lightweig
 
 INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Current_weight_class, Height, Reach)
 VALUES ('Mateusz Gamrot', 33, 'Poland', 'Striking', 'American Top Team', 'Lightweight', 70, 70);
+--Lightweight figherRecord inserts
+-- Insert for Arman Tsarukyan
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Arman Tsarukyan', 9, 5, 8, 0, 1, 0, 2, 0, 0, 0);
 
--- Featherweight
+-- Insert for Charles Oliveira
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Charles Oliveira', 10, 21, 3, 0, 4, 4, 2, 0, 0, 1);
+
+-- Insert for Justin Gaethje
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Justin Gaethje', 20, 1, 4, 0, 3, 2, 0, 0, 0, 0);
+
+-- Insert for Dustin Poirier
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Dustin Poirier', 15, 8, 7, 0, 3, 3, 2, 0, 0, 1);
+
+-- Insert for Mateusz Gamrot
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Mateusz Gamrot', 8, 5, 11, 0, 0, 0, 2, 0, 0, 1);
+
+
+
+
+
+-- Featherweight FIghter Inserts
 INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Current_weight_class, Height, Reach) --fmr champ
 VALUES ('Alexander Volkanovski', 35, 'Australia', 'Striking', 'City Kickboxing', 'Featherweight', 66, 71);
 
@@ -94,8 +219,29 @@ VALUES ('Yair Rodriguez', 31, 'Mexico', 'Striking', 'Jackson Wink MMA Academy', 
 
 INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Current_weight_class, Height, Reach)
 VALUES ('Movsar Evloev', 29, 'Russia', 'Striking', 'Tiger Muay Thai', 'Featherweight', 69, 72);
+--featherweigth fighter record inserts
+-- Insert for Alexander Volkanovski
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Alexander Volkanovski', 13, 3, 10, 0, 3, 0, 1, 0, 0, 0);
 
--- Bantamweight
+-- Insert for Max Holloway
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Max Holloway', 12, 2, 10, 0, 0, 1, 6, 0, 0, 0);
+
+-- Insert for Brian Ortega
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Brian Ortega', 3, 8, 5, 0, 2, 0, 1, 0, 0, 1);
+
+-- Insert for Yair Rodriguez
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Yair Rodriguez', 8, 5, 6, 0, 3, 1, 1, 0, 0, 1);
+
+-- Insert for Movsar Evloev
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Movsar Evloev', 3, 4, 11, 0, 0, 0, 0, 0, 0, 0);
+
+
+-- Bantamweight Fighter inserts
 INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Current_weight_class, Height, Reach)
 VALUES ('Marlon Vera', 30, 'Ecuador', 'Striking', 'RVCA Training Center', 'Bantamweight', 68, 70);
 
@@ -109,13 +255,40 @@ INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Current_weight_cla
 VALUES ('Merab Dvalishvili', 33, 'Georgia', 'Striking', 'Serra-Longo Fight Team', 'Bantamweight', 68, 68);
 
 INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Current_weight_class, Height, Reach)
-VALUES ('Ricky Simon', 31, 'USA', 'Striking', 'Gracie Barra Portland', 'Bantamweight', 68, 68);
+VALUES ('Ricky Simón', 31, 'USA', 'Striking', 'Gracie Barra Portland', 'Bantamweight', 68, 68);
 
 INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Current_weight_class, Height, Reach) --fmr double champ
 VALUES ('Henry Cejudo', 37, 'USA', 'Wrestling','Fight Ready','Bantamweight',66,64);
+--Bandtamweight fighterRecord inserts
+-- Insert for Marlon Vera
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Marlon Vera', 8, 10, 5, 0, 0, 0, 9, 0, 1, 0);
 
--- Flyweight
+-- Insert for Petr Yan
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Petr Yan', 7, 1, 9, 0, 0, 0, 4, 1, 0, 0);
 
+-- Insert for Cory Sandhagen
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Cory Sandhagen', 7, 3, 7, 0, 0, 1, 3, 0, 0, 0);
+
+-- Insert for Merab Dvalishvili
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Merab Dvalishvili', 3, 1, 13, 0, 0, 1, 3, 0, 0, 0);
+
+-- Insert for Ricky Simon
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Ricky Simón', 6, 4, 10, 0, 2, 1, 2, 0, 0, 0);
+
+-- Insert for Henry Cejudo
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Henry Cejudo', 8, 0, 8, 0, 1, 0, 3, 0, 0, 0);
+
+
+
+
+
+-- Flyweight Fighter Inserts
 INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Current_weight_class, Height, Reach)
 VALUES ('Brandon Moreno', 30, 'Mexico', 'Brazilian Jiu-Jitsu', 'Entram Gym', 'Flyweight', 67, 70);
 
@@ -123,10 +296,30 @@ INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Current_weight_cla
 VALUES ('Kai Kara-France', 31, 'New Zealand', 'Striking', 'City Kickboxing', 'Flyweight', 64, 68);
 
 INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Current_weight_class, Height, Reach)
-VALUES ('Manel Kape', 30, 'Angola', 'Striking', 'AKA Thailand', 'Flyweight', 65, 68);
+VALUES ('Manel Kape', 30, 'Portugal', 'Striking', 'AKA Thailand', 'Flyweight', 65, 68);
 
 INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Current_weight_class, Height, Reach)
 VALUES ('Brandon Royval', 31, 'USA', 'Brazilian Jiu-Jitsu', 'Factory X', 'Flyweight', 70, 70);
+--Flyyweight FIghterRecord inserts
+-- Insert for Brandon Moreno
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Brandon Moreno', 5, 11, 5, 0, 0, 0, 8, 0, 2, 0);
+
+-- Insert for Kai Kara-France
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Kai Kara-France', 11, 3, 10, 0, 3, 3, 5, 0, 0, 1);
+
+-- Insert for Manel Kape
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Manel Kape', 11, 5, 3, 0, 0, 2, 4, 0, 0, 0);
+
+-- Insert for Brandon Royval
+INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
+VALUES ('Brandon Royval', 4, 9, 3, 0, 1, 1, 5, 0, 0, 0);
+
+
+
+
 
 -- Women's Strawweight
 INSERT INTO Fighter (Name, Age, Nationality, Style, Gym_Name, Current_weight_class, Height, Reach)
@@ -177,25 +370,6 @@ VALUES ('Kayla Harrison',33,'USA','Judo','American Top Team','WBantamweight',68,
 
 
 
---Insert into fighter record
---Heavyweight
--- Insert for Tom Aspinall
-INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
-VALUES ('Tom Aspinall', 11, 3, 0, 0, 1, 1, 0, 1, 0, 0);
 
--- Insert for Ciryl Gane
-INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
-VALUES ('Ciryl Gane', 6, 3, 3, 0, 1, 0, 1, 0, 0, 0);
 
--- Insert for Sergei Pavlovich
-INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
-VALUES ('Sergei Pavlovich', 15, 0, 3, 0, 2, 0, 0, 0, 0, 0);
-
--- Insert for Stipe Miocic
-INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
-VALUES ('Stipe Miocic', 15, 0, 5, 0, 2, 0, 2, 0, 0, 0);
-
--- Insert for Curtis Blaydes
-INSERT INTO FighterRecord (FighterName, KOWins, SubWins, DecWins, DQWins, KOLosses, SubLosses, DecLosses, DQLosses, Draws, NoContests)
-VALUES ('Curtis Blaydes', 13, 0, 5, 0, 4, 0, 0, 0, 0, 1);
 
